b57710d76cb86986a35dff0418c34e33fd4f44ac
Christopher Savan <cxs7700@rit.edu>
2020-02-15 15:40:13 -0500

    test

--- Doggo.js
+++ Doggo.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 
 const Doggo = props => {
   
   
   
   return (
-    
+    <Image />
   )
 }
 
