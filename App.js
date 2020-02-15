b0ca2ddd83f071e922aacde745ff45e742547880
Christopher Savan <cxs7700@rit.edu>
2020-02-15 16:19:56 -0500

    Weather card starter

--- App.js
+++ App.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import HeaderCard from './Components/HeaderCard';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 
 export default function App() {
+  
+  
   return (
-    <View style={styles.container}>
-      <Text>Hey!</Text>
-
-      <HeaderCard 
-        title='New Day'
-        source={require('./assets/background16.jpg')}
-      />
-
-      <HeaderCard 
-        title='Past Days'
-        source={require('./assets/background9.jpg')}
-      />
-
     
-    </View>
   );
 }
 
