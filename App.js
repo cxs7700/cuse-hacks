fd77a9d4315bad774f9bd6e80364aac004fff5df
Zaarib <zz3553@g.rit.edu>
2020-02-15 20:40:21 -0500

    nav cards implemented

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , ScrollView} from 'react-native';
 import Home from './Components/Home';
 
 
 export default function App() {
   return (
-    <Home />
+    <ScrollView style={styles.container}>
+      <Home />
+    </ScrollView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    // backgroundColor: 'gray',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
--- Components/Home.js
+++ Components/Home.js
@@ -1,40 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , ScrollView} from 'react-native';
 import HeaderCard from './HeaderCard';
 
 export default function Home() {
     return (
-      <View style={styles.container}>
-        <Text>Hey!</Text>
-        
-        <View style={styles.cardLayout}>
+    <View style={styles.back}>
+        <Text style={styles.text}>Hey, User!</Text>
 
-            <HeaderCard
-            title='New Day'
-            source={require('../assets/background16.jpg')}
-            />
-    
-            <HeaderCard 
-            title='Past Days'
-            source={require('../assets/background9.jpg')}
-            />
+        <View style={styles.container}>
+            
+            <View style={styles.cardLayout}>
+
+                <HeaderCard
+                title='New Day'
+                source={require('../assets/background16.jpg')}
+                />
+                
+                <HeaderCard 
+                title='Past Days'
+                source={require('../assets/background9.jpg')}
+                />
+            </View>
+        
         </View>
-      
-      </View>
+    </View>
     );
   }
 
 
   const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      flexDirection: 'row',
-      backgroundColor: '#fff',
-      alignItems: 'flex-start',
-      justifyContent: 'flex-start',
+    //   backgroundColor: 'black',
+      width: '100%'
     },
     cardLayout: {
+        flex: 1,
         flexDirection: 'row',
-    }
+        justifyContent: 'space-evenly',
+        width: '90%',
+        display: 'flex',
+    },
+    text: {
+        fontSize: 25,
+        marginTop: 70,
+        marginLeft: 20,
+        color: 'black'
+    },
+
   });
   
