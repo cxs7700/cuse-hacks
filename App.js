d7513dff6ce777f0881983f892f06106866edb32
Christopher Savan <cxs7700@rit.edu>
2020-02-15 23:46:09 -0500

    merge conflict

--- App.js
+++ App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-<<<<<<< HEAD
-import { StyleSheet, Text, View } from 'react-native';
-import HeaderCard from './Components/HeaderCard';
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-=======
 import { StyleSheet, Text, View , ScrollView} from 'react-native';
 import Home from './Components/Home';
->>>>>>> 095aed07e2464a3a5b638df57fd07a41a43f1ceb
 
 
 export default function App() {
   
   
   return (
-<<<<<<< HEAD
-=======
     <ScrollView style={styles.container}>
       <Home />
     </ScrollView>
->>>>>>> 095aed07e2464a3a5b638df57fd07a41a43f1ceb
     
   );
 }
