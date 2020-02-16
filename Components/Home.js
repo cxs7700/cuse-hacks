a51bf725dad7061ee97b9d40b9b87d6d6a722fd5
Zaarib <zz3553@g.rit.edu>
2020-02-16 04:28:01 -0500

    started working on firebase

--- Components/Home.js
+++ Components/Home.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View , TouchableOpacity} from 'react-native';
 import HeaderCard from './HeaderCard';
 import {Navigation} from '@react-navigation/native';
 import Pastday from '../Components/Pastday';
+import firebase from 'firebase';
 
 
 const navigate = ({ Navigation }) => {
@@ -11,14 +12,27 @@ const navigate = ({ Navigation }) => {
 
 export default class Home extends React.Component {
 
+    componentWillMount() {
+        var firebaseConfig = {
+            apiKey: "AIzaSyBhGWu0c7QeM4VpBCa9g4-CRyw6vA3qSh4",
+            authDomain: "slate-8d1bb.firebaseapp.com",
+            databaseURL: "https://slate-8d1bb.firebaseio.com",
+            projectId: "slate-8d1bb",
+            storageBucket: "slate-8d1bb.appspot.com",
+            messagingSenderId: "32453220367",
+            appId: "1:32453220367:web:7925f428e7ef850ec3eb7a",
+            measurementId: "G-6NCCQCB780"
+          };
+          // Initialize Firebase
+          firebase.initializeApp(firebaseConfig);
+          firebase.analytics();
+    }
+
+
     constructor (props) {
         super(props);
         this.state={};
 
-    }
-
-    _onPressButton = () => {
-        alert('Button presses!');
     }
 
     render() {
@@ -37,11 +51,7 @@ export default class Home extends React.Component {
                         />
                     </TouchableOpacity>
 
-<<<<<<< HEAD
-                    <TouchableOpacity onPress={this.props.navigation.navigate("Pastdays")}>
-=======
                     <TouchableOpacity onPress={() => {this.props.navigation.navigate('Pastdays')}}>
->>>>>>> 64bfa5186a7209edb520a2268198a43f41db4ffd
                         <HeaderCard 
                         title='Past Days'
                         source={require('../assets/background9.jpg')}
